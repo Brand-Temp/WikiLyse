ef95f56458dff12e960460276b39585941c5c05e
Brand-Temp <brandon.temple.97@gmail.com>
2019-07-30 11:38:52 +1000

    Misc Adjustments

--- backend/typecorrection.js
+++ backend/typecorrection.js
@@ -1,29 +1,44 @@
 var file = cat('data/admin_active.txt');
 var users = file.split('\n');
+print('Now processing admin_active...');
 for(var i = 0; i < users.length; i++) {
   db.revisions.updateMany({'user':users[i]}, {$set: {'usertype':'admin'}});
 }
+print('Finished at:')
+print(new Date());
 
 file = cat('data/admin_former.txt');
 users = file.split('\n');
+print('Now processing admin_former...');
 for(var i = 0; i < users.length; i++) {
   db.revisions.updateMany({'user':users[i]}, {$set: {'usertype':'admin'}});
 }
+print('Finished at:')
+print(new Date());
 
 file = cat('data/admin_inactive.txt');
 users = file.split('\n');
+print('Now processing admin_inactive...');
 for(var i = 0; i < users.length; i++) {
   db.revisions.updateMany({'user':users[i]}, {$set: {'usertype':'admin'}});
 }
+print('Finished at:')
+print(new Date());
 
 file = cat('data/admin_semi_active.txt');
 users = file.split('\n');
+print('Now processing admin_semi_active...');
 for(var i = 0; i < users.length; i++) {
   db.revisions.updateMany({'user':users[i]}, {$set: {'usertype':'admin'}});
 }
+print('Finished at:')
+print(new Date());
 
 file = cat('data/bot.txt');
 users = file.split('\n');
+print('Now processing bot...');  
 for(var i = 0; i < users.length; i++) {
   db.revisions.updateMany({'user':users[i]}, {$set: {'usertype':'bot'}});
 }
+print('Finished at:')
+print(new Date());
