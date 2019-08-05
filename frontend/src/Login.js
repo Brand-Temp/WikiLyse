8c83b03f4c2f8956d1e4411385379a9e70f38c55
Brand-Temp <brandon.temple.97@gmail.com>
2019-08-06 09:59:29 +1000

    Added scaffold for login submit

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,11 +3,22 @@ import logo from './logo.svg';
 import './Login.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.submit.bind(this);
+  }
+
+  submit(event) {
+    alert('Submission!');
+    event.preventDefault();
+  }
+
   render() {
     return(
       <div class="content-wrapper">
         <div class="content">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input type="text" name="username" class="formbox" placeholder="Username"></input><br />
             <input type="password" name="password" class="formbox" placeholder="Password"></input><br />
             <button type="submit" class="formbutton">Login</button>
