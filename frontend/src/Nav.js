4d05fbfd850fdb098e43a9b012844d6e14490d03
Brand-Temp <brandon.temple.97@gmail.com>
2019-08-06 09:08:05 +1000

    Added navbar shrinking/expanding

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,21 +4,38 @@ import logo from './logo.svg';
 import './Nav.css';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {nav: 'nav-item-small', logo: 'logo-small'};
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scroll.bind(this));
+  }
+
+  scroll() {
+    if(window.pageYOffset > 80) {
+      this.setState({nav: 'nav-item-small', logo: 'logo-small'});
+    } else {
+      this.setState({nav: 'nav-item-large', logo: 'logo-large'});
+    }
+  }
+
   render() {
     return(
       <div class="nav">
         <div class="nav-analytics">
-          <Link to="/overall" class="nav-item">Overall</Link>
-          <Link to="/article" class="nav-item">Article</Link>
-          <Link to="/authors" class="nav-item">Authors</Link>
+          <Link to="/overall" class={this.state.nav}>Overall</Link>
+          <Link to="/article" class={this.state.nav}>Article</Link>
+          <Link to="/authors" class={this.state.nav}>Authors</Link>
         </div>
         <div class="nav-logo">
-          <Link to="/" class="nav-item" id="logo">WikiLyse</Link>
+          <Link to="/" class={this.state.nav} id={this.state.logo}>WikiLyse</Link>
         </div>
         <div class="nav-user">
-          <Link to="/login" class="nav-item">Login</Link>
-          <Link to="/signup" class="nav-item">Signup</Link>
-          <Link to="/logout" class="nav-item">Logout</Link>
+          <Link to="/login" class={this.state.nav}>Login</Link>
+          <Link to="/signup" class={this.state.nav}>Signup</Link>
+          <Link to="/logout" class={this.state.nav}>Logout</Link>
         </div>
       </div>
     );
