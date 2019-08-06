aa640e33d18fd887efe5d4661865e13d0f102765
Brand-Temp <brandon.temple.97@gmail.com>
2019-08-06 10:27:14 +1000

    Basic expand-shrink on text input fields for login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,8 +5,11 @@ import './Login.css';
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {u:'formbox-blur', p:'formbox-blur'};
 
     this.handleSubmit = this.submit.bind(this);
+    this.focus = this.focus;
+    this.blur = this.blur;
   }
 
   submit(event) {
@@ -14,13 +17,27 @@ class Login extends Component {
     event.preventDefault();
   }
 
+  focus(box) {
+    if(box === 'u')
+      this.setState({u: 'formbox-focus'});
+    if(box === 'p')
+      this.setState({p: 'formbox-focus'})
+  }
+
+  blur(box) {
+    if(box === 'u')
+      this.setState({u: 'formbox-blur'});
+    if(box === 'p')
+      this.setState({p: 'formbox-blur'})
+  }
+
   render() {
     return(
       <div class="content-wrapper">
         <div class="content">
           <form onSubmit={this.handleSubmit}>
-            <input type="text" name="username" class="formbox" placeholder="Username"></input><br />
-            <input type="password" name="password" class="formbox" placeholder="Password"></input><br />
+            <input type="text" name="username" class={this.state.u} placeholder="Username" onFocus={this.focus.bind(this, 'u')} onBlur={this.blur.bind(this, 'u')}></input><br />
+            <input type="password" name="password" class={this.state.p} placeholder="Password" onFocus={this.focus.bind(this, 'p')} onBlur={this.blur.bind(this, 'p')}></input><br />
             <button type="submit" class="formbutton">Login</button>
           </form>
         </div>
