d184b648651276a0eb65fca8445063dca62adb2d
Brand-Temp <brandon.temple.97@gmail.com>
2019-08-28 17:43:56 +1000

    Placeholder GET route for login page

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -5,10 +5,7 @@ const controller =require('../controllers/loginController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.session.user!=null){
-    res.render('error', {message:"You have logged in.",flag:1,name:req.session.user.name});
-  }
-  return res.render('../views/login.ejs',{flag:0});
+  res.send({message: 'This route is set to be removed...'});
 });
 
 router.post('/', controller.login);
