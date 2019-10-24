fa34103d8cdaebbb5d7ead719fb1a79e8c677c02
Brand-Temp <brandon.temple.97@gmail.com>
2019-10-24 15:19:01 +1100

    Fixed label for password and hashing

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Signup.css';
+import crypto from 'crypto';
 
 class Signup extends Component {
   constructor(props) {
@@ -31,18 +32,25 @@ class Signup extends Component {
   }
 
   change(event) {
-    let name = event.target.name;
-    let value = event.target.value;
-    this.setState({[name]:value});
-    if(name == 'password') {
-      if(value.length < 8) {
-        this.setState({errors:{password:"Password must be at least 8 characters. <br />"}});
-        this.setState({passwordValid: false});
-      } else {
-        this.setState({errors:{password:''}});
-        this.setState({passwordValid: true});
+    const name = event.target.name;
+
+    if (name === 'password') {
+      const valueLen = event.target.value.length;
+      const hash = crypto.createHash('sha512');
+      hash.write(event.target.value);
+      const value = hash.digest('hex');
+      this.setState({password:value});
+      if(name === 'password') {
+        if(valueLen < 8 && valueLen > 0) {
+          this.setState({errors:{password:'Password must be at least 8 characters.'}});
+          this.setState({passwordValid: false});
+        } else {
+          this.setState({errors:{password:''}});
+          this.setState({passwordValid: true});
+        }
       }
     }
+    
     event.preventDefault();
   }
 
@@ -54,11 +62,10 @@ class Signup extends Component {
               <input type="text" class="formbox" placeholder="Username"></input><br />
             <input type="text" class="formbox" placeholder="Email"></input><br />
             <input type="text" class="formbox" placeholder="Confirm email"></input><br />
-            <label>
-              
-              {this.state.errors.password}
-              <input name="password" type="password" class="formbox" placeholder="Password" onChange={this.change}></input><br />
+            <label for="password">
+              {this.state.errors.password} <br />
             </label>
+            <input name="password" type="password" class="formbox" placeholder="Password" onChange={this.change}></input><br />
             <button type="submit" class="formbutton">Signup</button>
           </form>
         </div>
