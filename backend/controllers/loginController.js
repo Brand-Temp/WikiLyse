7ff56d9555f5159a7b1aabb8fdf988afe14cf02f
Brand-Temp <brandon.temple.97@gmail.com>
2020-10-31 19:17:51 +1100

    Updating login controller

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -2,22 +2,14 @@ const crypto =require('crypto');
 const User =require('../models/user');
 
 exports.login = function(req, res, next) {
-  const md5 =crypto.createHash('md5');
-  const newPas = md5.update(req.body.password).digest('hex');
-  const postData ={
-    username: req.body.username,
-    password: newPas,
-  };
-  console.log(postData.username);
-  User.findOne({username: postData.username}, function(err, data) {
+  const uname = req.data.username;
+  const paswd = req.data.password;
+  User.findOne({username: uname, password: paswd}, function(err, data) {
     if (data) {
-      const user={
-        name: data.firstname
-      };
-      req.session.user =user;
-      res.send({message: 'That worked!!', sess_id:req.session.id});
+      req.session.user = data;
+      res.send({message: 0, sess_id:req.session.id});
     } else {
-      res.send({mesage: 'That did not work'});
+      res.send({mesage: 1});
     }
   });
 };
